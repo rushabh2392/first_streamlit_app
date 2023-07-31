0dfcf020db8aae7d4e463fe46d7c0ff4d2b6609f
rushabh2392 <41894790+rushabh2392@users.noreply.github.com>
2023-07-31 15:07:15 +0530

    Update streamlit_app.py
    bud in dinner "

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title("My paraent new healthy diner')
+streamlit.title("My paraent new healthy diner")
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
