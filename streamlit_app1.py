4f637dcefe99c80c2e0a571e2e83abbc6d48a261
rushabh2392 <41894790+rushabh2392@users.noreply.github.com>
2023-08-07 16:32:31 +0530

    Update streamlit_app1.py
    new details
    1

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title("My Mom's new healthy diner")
 
@@ -18,3 +19,5 @@ streamlit.dataframe(fruits_to_show)
 
 
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
