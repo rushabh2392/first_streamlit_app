ee50f17d90069ef09c22e8ca2fd780d8aa04ca75
rushabh2392 <41894790+rushabh2392@users.noreply.github.com>
2023-08-07 16:36:05 +0530

    Update streamlit_app1.py
    spacing

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -20,6 +20,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
+
 
